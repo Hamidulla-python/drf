70548141c47f70baee63db9d2933f1a589566bd3
Hamidulla-python <habdreshitov@gmail.com>
2023-08-15 20:45:29 +0500

    data-base

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Jobs
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Jobs
+#         fields = ['job_title', 'min_salary', 'max_salary']
 
--- drf_api/views.py
+++ drf_api/views.py
@@ -3,14 +3,11 @@
 # Create your views here.
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework import permissions
-from .serializers import UserSerializer
+#from .serializers import UserSerializer
+from .models import Jobs
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    #permission_classes = [permissions.IsAuthenticated]
+# class UserViewSet(viewsets.ModelViewSet):
+#
+#     queryset = Jobs.objects.all()
+#     serializer_class = UserSerializer
